2bd2f1bb6171a82309384ee331366c28f260f317
405516_Gonzalez_Federico <405516@tecnicatura.frc.utn.edu.ar>
2024-10-25 17:43:11 -0300

    Revert "Update access-visitors-register-service.service.ts"
    This reverts commit b3304907e4d09b858a0b07f92e4aec20335988f1.

--- accesses/src/app/services/visitors/access-visitors-register-service/access-visitors-register-service/access-visitors-register-service.service.ts
+++ accesses/src/app/services/visitors/access-visitors-register-service/access-visitors-register-service/access-visitors-register-service.service.ts
@@ -1,29 +1,16 @@
 import { Injectable } from '@angular/core';
-import { AllowedDay, Visitor, AuthRange } from '../../../../models/visitors/VisitorsModels';
+import { AllowedDay, Visitor } from '../../../../models/visitors/VisitorsModels';
 import { Observable, BehaviorSubject } from 'rxjs';
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class AccessVisitorsRegisterServiceService {
+
   private visitors: Visitor[] = [];
   private visitorsTemporalsSubject = new BehaviorSubject<Visitor[]>([]);
 
   private allowedDays: AllowedDay[] = [];
   private allowedDaysSubject = new BehaviorSubject<AllowedDay[]>([]);
-
-  private authRange: AuthRange | null = null;
-  private authRangeSubject = new BehaviorSubject<AuthRange | null>(null);
-
-  getAuthRange(): Observable<AuthRange | null> {
-    return this.authRangeSubject.asObservable();
-  }
-
-  setAuthRange(authRange: AuthRange): void {
-    this.authRange = authRange;
-    this.authRangeSubject.next(this.authRange);
-  }
 
   getAllowedDays(): Observable<AllowedDay[]> {
     return this.allowedDaysSubject.asObservable();
@@ -39,23 +26,9 @@ export class AccessVisitorsRegisterServiceService {
     this.allowedDaysSubject.next(this.allowedDays);
   }
 
-  addVisitorsTemporalsSubject(visitor: Visitor): boolean {
-    // Validar si el documento o la patente ya existen
-    const documentExists = this.visitors.some(v => v.document === visitor.document);
-    const licensePlateExists = this.visitors.some(v => v.vehicle?.licensePlate === visitor.vehicle?.licensePlate);
-
-    if (documentExists) {
-      return false; // Retornar false si el documento ya existe
-    }
-
-    if (licensePlateExists) {
-      return false; // Retornar false si la patente ya existe
-    }
-
-    // Agregar el visitante si no existen documentos ni patentes duplicadas
+  addVisitorsTemporalsSubject(visitor: Visitor): void {
     this.visitors.push({ ...visitor });
     this.visitorsTemporalsSubject.next(this.visitors);
-    return true; // Retornar true si se agregó el visitante
   }
 
   deleteVisitorsTemporalsSubject(visitor: Visitor): void {
@@ -78,22 +51,12 @@ export class AccessVisitorsRegisterServiceService {
   }
 
   updateVisitorsTemporalsSubject(visitorToUpdated: Visitor): void {
-    const index = this.visitors.findIndex(v => v.document === visitorToUpdated.document);
-    if (index !== -1) {
-      this.visitors[index] = { ...visitorToUpdated };
+    const indice = this.visitors.findIndex(v => v.document === visitorToUpdated.document);
+    if (indice !== -1) {
+      this.visitors[indice] = { ...visitorToUpdated };
       this.visitorsTemporalsSubject.next([...this.visitors]);
     } else {
       console.log('Visitante no encontrado para modificar:', visitorToUpdated);
     }
-  }
-  clearVisitorsTemporalsSubject(): void {
-    this.visitorsTemporalsSubject.next([]);
-  }
-  clearAllowedDayTemporalsSubject(): void {
-    this.allowedDaysSubject.next([]);
-  }
-
-  clearAuthRange(): void {
-    this.authRangeSubject.next(null);
   }
 }
