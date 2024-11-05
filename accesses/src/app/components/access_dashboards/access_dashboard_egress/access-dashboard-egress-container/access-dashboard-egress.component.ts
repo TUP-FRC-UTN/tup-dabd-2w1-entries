ae664b0d9ba562b04477107ed0783de880068303
405516_Gonzalez_Federico <405516@tecnicatura.frc.utn.edu.ar>
2024-11-04 22:52:08 -0300

    Create access-dashboard-egress.component.ts

---
+++ accesses/src/app/components/access_dashboards/access_dashboard_egress/access-dashboard-egress-container/access-dashboard-egress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-access-dashboard-egress',
+  standalone: true,
+  imports: [],
+  templateUrl: './access-dashboard-egress.component.html',
+  styleUrl: './access-dashboard-egress.component.css'
+})
+export class AccessDashboardEgressComponent {
+
+}
