bd884cadb082f6a2dfd833a1c3d9ed7b8c89e247
404888_MONASTERIO <404888@tecnicatura.frc.utn.edu.ar>
2024-11-10 23:18:20 -0300

    Fixed pdf export

--- accesses/src/app/services/access_report/access-export/export.service.ts
+++ accesses/src/app/services/access_report/access-export/export.service.ts
@@ -160,6 +160,14 @@ export class ExportService {
               return 'Desconocido';
             }
 
+            if (column === 2) {
+              const titleMatch = strData.match(/data-title="([^"]+)"/);
+              if (titleMatch && titleMatch[1]) {
+                return titleMatch[1];
+              }
+              return 'Desconocido';
+            }
+
             // Para la columna con los guiones
             if (column === 8) {
               // Si incluye el div con clase text-center
